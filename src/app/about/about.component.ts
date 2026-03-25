9130a6cbccca361bdfb429bdb0bc657b3c882d4b
Harshith <harshith.ml.2026@gmail.com>
2026-03-25 18:25:25 -0400

    image zoom

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,30 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
+  diagramOpen = false;
+  zoom = 1;
+
   constructor(private router: Router) {}
+
   goHome() { this.router.navigate(['/']); }
+
+  openDiagram() {
+    this.zoom = 1;
+    this.diagramOpen = true;
+  }
+
+  closeDiagram() {
+    this.diagramOpen = false;
+  }
+
+  onWheel(event: WheelEvent) {
+    event.preventDefault();
+    const delta = event.deltaY > 0 ? -0.1 : 0.1;
+    this.zoom = Math.min(5, Math.max(0.5, this.zoom + delta));
+  }
+
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    if (this.diagramOpen) this.closeDiagram();
+  }
 }
